695f7539f9720d20911440d0d7a75eb94b7d4da0
fazlicodes <fazlimaam@gmail.com>
2025-03-31 22:37:49 +0400

    upd

--- src/data/experience/malt.xp.ts
+++ src/data/experience/malt.xp.ts
@@ -4,16 +4,16 @@ import { differenceInMonths } from 'date-fns';
 import { Experience } from 'models/xp.model';
 
 export const maltXp: Experience = {
-  id: 'aldhalhd',
+  id: 'mbzuai',
   logo: 'https://fazliimam.github.io/images/mbzuai.png',
   description: ``,
   title: 'MBZUAI',
-  technos: [TechnosList.Vue, TechnosList.Typescript, TechnosList.Jest],
+  technos: [TechnosList.Python, TechnosList.Pytorch, TechnosList.Tensorflow, TechnosList.Scholar],
   year: 2024,
   type: ExperienceType.Job,
   duration: differenceInMonths(new Date(), new Date(2024, 7, 1)) + 1,
   currentPosition: true,
   role: 'Machine Learning Researcher',
-  links: [{ link: 'https://fazliimam.github.io/', title: 'Site MBZUAI' }],
+  links: [{ link: 'https://fazliimam.github.io/', title: 'MBZUAI' }],
   location: 'Abu Dhabi, UAE',
 };
