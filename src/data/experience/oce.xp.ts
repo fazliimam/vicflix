ba2ee77e4e7e61b2d0520742c0ea340c894aae86
fazlicodes <fazlimaam@gmail.com>
2025-04-01 11:51:41 +0400

    upd

--- src/data/experience/oce.xp.ts
+++ src/data/experience/oce.xp.ts
@@ -5,7 +5,7 @@ import { Experience } from 'models/xp.model';
 
 export const oceXp: Experience = {
   id: 'OnqllpoI',
-  logo: 'https://firebasestorage.googleapis.com/v0/b/vicflix-2fbe0.appspot.com/o/Pictures%2FExperiences%2Foce%20logo.png?alt=media&token=b33d5af0-c384-432b-bf42-0bf86d5658c7',
+  logo: 'https://k2-prod-alm.s3.us-east-1.amazonaws.com/brightspot/52/24/cdc7ec5a4f588909b17bd978939c/stax-master-frame-2.png',
   description: `Océ Printing Technologies est ma première expérience professionnelle en tant que développeur.
   
   Dans le cadre de mon BTS SIO, j'ai du réaliser un stage d'une durée de 2 mois durant lesquels j'ai pu découvrir des technologies telles qu'angularjs et Typescript.
