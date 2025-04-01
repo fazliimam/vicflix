b3efbe1ffd5bdaa0db1ea526488a6e818726c8e3
fazlicodes <fazlimaam@gmail.com>
2025-04-01 16:11:45 +0400

    upd

--- src/data/projects/milinus.project.ts
+++ src/data/projects/milinus.project.ts
@@ -1,12 +1,11 @@
 import { TechnosList } from '@constants';
 import { AppTrads, Project, ProjectContext, ProjectType } from '@models';
 import { milinusVideos } from '../videos/milinus.videos';
-import { milinusImage } from '../videos/milinus';
-
 
 export const milinusProject: Project = {
   title: 'Milinus Admin',
-  picture: milinusImage,  
+  picture:
+  'https://github.com/fazliimam/fazliimam.github.io/blob/master/images/milinus.png',
   placeholder:
     'https://firebasestorage.googleapis.com/v0/b/vicflix-2fbe0.appspot.com/o/Pictures%2FProjects%2Fmilinus%2Fmilinus_placeholder.png?alt=media&token=5568a432-acbd-46da-a80e-5b41a3c3b121',
   slogan: AppTrads.ProjectDescriptions.Milinus.Slogan,
